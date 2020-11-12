a17be9e68f658cf42b9aa953605b54fc6214b192
Zihang97 <52614651+Zihang97@users.noreply.github.com>
2020-11-12 21:13:01 +0800

    Update test_nlp.py

--- Src/test_nlp.py
+++ Src/test_nlp.py
@@ -23,19 +23,3 @@ class Testnlp:
 	def test_analyze_sentiment_negative2(self):
 		assert analyze_sentiment("It's rainy outside")[0] < 0
 		
-class Testmovieanalyzer:
-	def test_movie_analyzer_no_input(self):
-		with pytest.raises(TypeError):
-			movie_analyzer()
-
-	def test_movie_analyzer_Hocus_Pocus(self):
-		assert movie_analyzer("Hocus Pocus",10) < 5
-		
-	def test_movie_analyzer_Unhinged(self):
-		assert movie_analyzer("Unhinged",10) < 5
-		
-	def test_movie_analyzer_Tenet(self):
-		assert movie_analyzer("Tenet",10) > 5
-		
-	def test_movie_analyzer_Kajillionaire(self):
-		assert movie_analyzer("Kajillionaire",10) > 5
